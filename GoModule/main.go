ddba089e66919c3afd32fdc39a1058aa6475d305
aneri2610 <anerishah36@gmail.com>
2018-11-13 18:23:18 +0530

    main file modify

--- GoModule/main.go
+++ GoModule/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/AneriShah2610/GoGeek/GoModule/mo"
+
 	log "github.com/sirupsen/logrus"
 )
 
@@ -10,5 +12,7 @@ func main() {
 	fmt.Println("hi")
 	log.Warn("It's a warn log")
 	log.Info("It's a info log")
+
+	mo.Mo()
 	log.Fatalf("It's a fatal log")
 }
