5fed41ceab3770619e343adcbc5b133db9961c39
aneri2610 <anerishah36@gmail.com>
2018-11-15 16:07:34 +0530

    main.go file altered

--- cockroachdb/main.go
+++ cockroachdb/main.go
@@ -7,7 +7,10 @@ import (
 )
 
 func main() {
+	// Code is a subpackga eof cocckroach db and it has a simple cockroachdb example with go
 	code.BasicCockroachdbCode()
-	//Gorm.CockroachdbWithGorm()
+	//Gorm is a subpackga eof cocckroach db and it use GORM+Go+Cockroachdb and db.AutoMigrate method to auto create table
+	Gorm.CockroachdbWithGorm()
+	//Gorm is a subpackga eof cocckroach db and it use GORM+Go+Cockroachdb and db.CreateTable method to create table
 	Gorm.GORMWithCockroach()
 }
