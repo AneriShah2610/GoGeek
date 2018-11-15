c6b5e2acd25e1c4fd71e96d5808bf684eba6f102
aneri2610 <anerishah36@gmail.com>
2018-11-15 16:56:14 +0530

    modify file

--- cockroachdb/Gorm/gORMwithCockroach.go
+++ cockroachdb/Gorm/gORMwithCockroach.go
@@ -19,8 +19,12 @@ func GORMWithCockroach() {
 	defer db.Close()
 
 	// Migrate the schema
-	db.CreateTable(&Product{})
+	//db.CreateTable(&Product{})
 
 	// Create
-	db.Create(&Product{Code: 001, Price: 1000})
+	//db.Create(&Product{Code: 001, Price: 1000})
+	var product Product
+	db.Model(&product).Update("code", 003)
+	db.Model(&product).Update("price", 2000)
+	db.Save(&product)
 }
