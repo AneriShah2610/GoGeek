7163ac3d4dcb1bd152f791511ee2837dad238b87
aneri2610 <anerishah36@gmail.com>
2018-11-15 13:00:54 +0530

    main file modified

--- cockroachdb/main.go
+++ cockroachdb/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/AneriShah2610/GoGeek/cockroachdb/Gorm"
 	"github.com/AneriShah2610/GoGeek/cockroachdb/code"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	code.BasicCockroachdbCode()
+	Gorm.CockroachdbWithGorm()
 }
