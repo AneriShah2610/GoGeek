3ea49f1f1e7e62ed9d1d7f4735d28fe6e7dff095
aneri2610 <anerishah36@gmail.com>
2018-11-22 18:37:52 +0530

    main file add

--- collegeapp/app/main.go
+++ collegeapp/app/main.go
@@ -11,8 +11,8 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/college", api.GetCollege).Methods("GET")
-	router.HandleFunc("/college", api.CreateCollege).Methods("POST")
-	router.HandleFunc("/college/{id}", api.DeleteCollege).Methods("DELETE")
-	router.HandleFunc("/college/{id}", api.UpdateCollege).Methods("PUT")
+	//router.HandleFunc("/college", api.CreateCollege).Methods("POST")
+	//router.HandleFunc("/college/{id}", api.DeleteCollege).Methods("DELETE")
+	//router.HandleFunc("/college/{id}", api.UpdateCollege).Methods("PUT")
 	log.Fatal("error while routing : ", http.ListenAndServe(":8000", router))
 }
